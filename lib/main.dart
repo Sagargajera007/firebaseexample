a2a0af27c3f59540b002a6d8e40d7b6bc803b7bd
Sagar Gajera <sagar194@gmail.com>
2022-08-24 17:08:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebaseexample/emailpass.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+ void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  );
+  runApp(MaterialApp(home: emailpass()));
+}
